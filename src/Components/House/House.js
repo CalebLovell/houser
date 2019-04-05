e89c44d43a2f10f95fb74505bed4729b5a610d4f
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-04-05 10:41:01 -0600

    React up

---
+++ src/Components/House/House.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class House extends Component {
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div className="House">
+        House
+      </div>
+    );
+  }
+}
+
+export default House;
---
+++ src/Components/Wizard/Wizard.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Wizard extends Component {
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div className="Wizard">
+        Wizard
+      </div>
+    );
+  }
+}
+
+export default Wizard;
